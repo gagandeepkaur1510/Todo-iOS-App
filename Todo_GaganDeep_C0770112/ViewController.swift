1fb027f3ecf279c91ca5352d97989c7628304b2f
gagandeepkaur1510 <gagandeepkaur1510@gmail.com>
2020-06-27 23:43:12 -0400

    Added Basic TableView for Categories

--- Todo_GaganDeep_C0770112/ViewController.swift
+++ Todo_GaganDeep_C0770112/ViewController.swift
@@ -7,14 +7,67 @@
 //
 
 import UIKit
+import CoreData
 
-class ViewController: UIViewController {
-
+class ViewController: UITableViewController {
+    
+    var categories = [Category]()
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        loadCategories()
     }
-
-
+    
+    func loadCategories() {
+        let request: NSFetchRequest<Category> = Category.fetchRequest()
+        
+        do {
+            categories = try context.fetch(request)
+        } catch {
+            print("Error loading folders \(error.localizedDescription)")
+        }
+        var cond = true
+        for category in categories
+        {
+            if category.title == "Archived"
+            {
+                cond = false
+                break
+            }
+        }
+        if cond
+        {
+            let category = Category(context: context)
+            category.title = "Archived"
+            categories.append(category)
+            save()
+        }
+        tableView.reloadData()
+    }
+    
+    func save()
+    {
+        do {
+            try context.save()
+            tableView.reloadData()
+        } catch {
+            print("Error saving folders \(error.localizedDescription)")
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return categories.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCell(withIdentifier: "categoryCell")
+        if cell == nil
+        {
+            cell = UITableViewCell(style: .default, reuseIdentifier: "categoryCell")
+        }
+        cell?.textLabel?.text = categories[indexPath.row].title
+        return cell!
+    }
 }
 
